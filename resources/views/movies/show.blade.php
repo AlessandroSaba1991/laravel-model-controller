45d995fd3bcbe8d06550a6d6e8193dcba0f41f3e
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-06-13 16:47:13 +0200

    add css for show.blade

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,18 +1,21 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row row-cols-3">
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h2>{{$movie->title}}</h2>
-                    <h3>{{$movie->original_title}}</h3>
-                    <p>{{$movie->nationality}}</p>
-                    <p>{{$movie->date}}</p>
-                    <p>{{$movie->vote}}</p>
-                </div>
+<section class="movie py-5 h-100">
+    <div class="container h-100 d-flex align-items-center">
+        <div class="card w-100">
+            <div class="img_card">
+                <img height="300" src="{{$movie->img}}" alt="{{$movie->title}}">
+            </div>
+            <div class="card-body text-center">
+                <h2><span>Title</span><br>{{$movie->title}}</h2>
+                <h3><span>Original Title</span><br>{{$movie->original_title}}</h3>
+                <p><span>Overviews</span><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quibusdam delectus obcaecati. At enim itaque error blanditiis ab atque excepturi, tenetur soluta deserunt similique tempore earum nulla voluptates sed nemo quas repellat dolor odio ut praesentium sint sunt delectus possimus.</p>
+                <p><span>Nationality</span><br>{{$movie->nationality}}</p>
+
+                <p><span>Date Release: </span><br>{{$movie->date}}</p>
+                <p><span>Vote: </span><br>{{$movie->vote}}</p>
             </div>
         </div>
     </div>
-</div>
+</section>
 @endsection
